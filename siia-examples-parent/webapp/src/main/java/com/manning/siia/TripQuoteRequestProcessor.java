f974656591121d0e145e8a99d9bd0366f89d47b0
Neale.Upstone <neale.upstone@opencredo.com>
2011-01-26 13:59:23 +0000

    Formatting/typos

--- siia-examples-parent/webapp/src/main/java/com/manning/siia/TripQuoteRequestProcessor.java
+++ siia-examples-parent/webapp/src/main/java/com/manning/siia/TripQuoteRequestProcessor.java
@@ -6,7 +6,6 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.xml.transform.StringResult;
 import org.springframework.xml.transform.StringSource;
 
-import javax.xml.transform.Result;
 import javax.xml.transform.Source;
 
 
@@ -15,7 +14,7 @@ public class TripQuoteRequestProcessor {
     private Log logger = LogFactory.getLog(getClass());
 
     public Source processTripRequest(Source requestSource) throws Exception{
-        logger.info("Trip request recevied");
+        logger.info("Trip request received");
         StringResult res = new StringResult();
         res.getWriter().append("<ok/>");
         return new StringSource(res.toString());
